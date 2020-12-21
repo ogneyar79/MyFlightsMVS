f86750dfa02e1b6a132ec6d06eaae2679d220e08
ogneyar79 <maksimus.s@mail.ru>
2020-12-21 23:24:24 +0300

    Correct main class method main,Add list of contents

--- src/main/java/com/gridnine/testing/checkingFlight/Main.java
+++ src/main/java/com/gridnine/testing/checkingFlight/Main.java
@@ -11,11 +11,10 @@ import java.time.LocalDateTime;
 import java.util.List;
 import java.util.function.Predicate;
 
+/**
+ * Checking class to except from test list some flights.
+ */
 public class Main {
-
-    /**
-     * Checking class to except from test list some flights.
-     */
     public static void main(String... args) {
         LocalDateTime timeNow = LocalDateTime.now();
         List<Flight> flightList = FlightBuilder.createFlights();
@@ -23,13 +22,19 @@ public class Main {
         Predicate<Flight> predicate = new FlightAfter(timeNow);
 
         // We are excepting a flight departing in the past
+        System.out.println(" We are excepting flights that was at pass until date  timeNow: " + timeNow);
+        System.out.println();
         filter.filter(flightList, predicate).stream().forEach(System.out::println);
 
         //  //We are excepting a flight that departs before it arrives
+        System.out.println();
+        System.out.println(" We are excepting flights that was incorrect \"that departs before it arrives:\"");
         predicate = new IncorrectSegment();
         filter.filter(flightList, predicate).stream().forEach(System.out::println);
 
-        // with more than two hours ground time if change ">" on ">=" we have correct result.
+        // with more than two hours ground time
+        System.out.println();
+        System.out.println("We are excepting flights with more than two hours ground time: ");
         predicate = new WaitingTime(2);
         filter.filter(flightList, predicate).stream().forEach(System.out::println);
     }
